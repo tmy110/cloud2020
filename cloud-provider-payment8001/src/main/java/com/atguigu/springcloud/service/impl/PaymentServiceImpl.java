2f7f47cc2a59381d8a859571a69c42ecd6c5be4e
田满意 <18361036456@163.com>
2020-04-10 10:19:56 +0800

    支付接口搭建

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl {
+
+
+//    private PaymentDao paymentDao;
+
+
+
+}
+//@Service
+//public class PaymentServiceImpl implements PaymentService {
+//
+//            @Resource
+//    private PaymentDao paymentDao;
+//
+//            public int create(Payment payment){
+//                return paymentDao.create(payment);
+//            }
+//
+//            public Payment getPaymentById( Long id){
+//
+//                return paymentDao.getPaymentById(id);
+//
+//            }
+//        }

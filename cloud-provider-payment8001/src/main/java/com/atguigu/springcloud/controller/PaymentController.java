d7e666c5a41dfe873d4862382bb901a4a6af8564
田满意 <395278367@qq.com>
2020-04-13 23:48:14 +0800

    ---

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entitles.CommonResult;
 import com.atguigu.springcloud.entitles.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.discovery.DiscoveryClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +20,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+
     //http://127.0.0.1:8001/payment/create?serial=1234
     @PostMapping(value = "/payment/create")
     public CommonResult<Payment> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("********插入结果：" + result);
         if (result > 0) {
-            return new CommonResult(200, "插入结果,serverPort:"+serverPort, result);
+            return new CommonResult(200, "插入结果,serverPort:" + serverPort, result);
         } else {
             return new CommonResult(444, "插入失败", null);
         }
@@ -38,10 +44,16 @@ public class PaymentController {
         Payment paymentById = paymentService.getPaymentById(id);
         log.info("插入结果：" + paymentById + " 你好呀！");
         if (paymentById != null) {
-            return new CommonResult(200, "查询成功！,serverPort:"+serverPort, paymentById);
+            return new CommonResult(200, "查询成功！,serverPort:" + serverPort, paymentById);
         } else {
             return new CommonResult(444, "没有对应的记录！查询id：" + id, null);
         }
+    }
+    //服务发现 不管用
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+//        discoveryClient.getIn
+        return null;
     }
 
 }

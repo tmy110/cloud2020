3a2e8861b20f491ad871da34f53e7f9c57f0e898
田满意 <18361036456@163.com>
2020-04-15 17:10:13 +0800

    restTemplate.getForEntity使用

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entitles.CommonResult;
 import com.atguigu.springcloud.entitles.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderController {
-//    public static final String PAYMENT_URL = "http://127.0.0.1:8001"; 单击
+    //    public static final String PAYMENT_URL = "http://127.0.0.1:8001"; 单击
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     //CLOUD-PAYMENT-SERVICE
     @Resource
@@ -30,6 +31,18 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    //http://127.0.0.1:8000/consumer/payment/getForEntity/2
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败!");
+        }
     }
 
 }

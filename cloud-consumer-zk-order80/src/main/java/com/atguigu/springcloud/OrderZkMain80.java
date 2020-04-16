cdd2bcb2353dc904fc761d50c39186ebf6ebf103
田满意 <18361036456@163.com>
2020-04-16 17:12:39 +0800

    ---

--- cloud-consumer-zk-order80/src/main/java/com/atguigu/springcloud/OrderZkMain80.java
+++ cloud-consumer-zk-order80/src/main/java/com/atguigu/springcloud/OrderZkMain80.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient//该注解用于向使用consul或者zookeeper作为注册中心时注册服务
 public class OrderZkMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderZkMain80.class, args);

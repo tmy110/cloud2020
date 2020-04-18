f708d09ab4fc965809c590da83d569ca6f424edb
田满意 <395278367@qq.com>
2020-04-18 11:01:27 +0800

    cloud-consumer-feign-hystrix-order80

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,40 +1,45 @@
 package com.atguigu.springcloud.controller;
 
+import com.atguigu.springcloud.service.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @Slf4j
 public class OrderHystrixController {
 
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
 
+    /**
+     * 正常访问
+     * http://localhost:8000/consumer/payment/hystrix/ok/31
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        log.info("*****result:" + result);
+        return result;
+    }
 
-//
-//    /**
-//     * 正常访问
-//     * http://localhost:8001/payment/hystrix/ok/31
-//     * @param id
-//     * @return
-//     */
-//    @GetMapping("/payment/hystrix/ok/{id}")
-//    public String paymentInfo_OK(@PathVariable("id") Integer id) {
-//        String result = paymentService.paymentInfo_OK(id);
-//        log.info("*****result:" + result);
-//        return result;
-//    }
-//
-//    /**
-//     * 超时访问
-//     * http://localhost:8001/payment/hystrix/timeout/31
-//     * @param id
-//     * @return
-//     */
-//    @GetMapping("/payment/hystrix/timeout/{id}")
-//    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
-//        String result = paymentService.paymentInfo_TimeOut(id);
-//        log.info("*****result:" + result);
-//        return result;
-//    }
+    /**
+     * 超时访问
+     * http://localhost:8000/consumer/payment/hystrix/timeout/31
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        log.info("*****result:" + result);
+        return result;
+    }
 }

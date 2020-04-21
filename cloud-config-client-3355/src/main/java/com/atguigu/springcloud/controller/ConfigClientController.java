923ba1b1e5b2876ae4484b10fd513de98a94ccc9
田满意 <18361036456@163.com>
2020-04-21 17:28:51 +0800

    ---

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
+@RefreshScope //刷新远程配置的注解  需要发送post http://localhost:3355/actuator/refresh 请求
 public class ConfigClientController {
 
     @Value("${config.info}")
     private String configInfo;
+
     //http://localhost:3355/configInfo
     @GetMapping("/configInfo")
-    public String getConfigInfo(){
+    public String getConfigInfo() {
         return configInfo;
     }
 }
